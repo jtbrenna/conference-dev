f546042c2eb09789e40e35db989cbd1da62aeb16
Jack Brennan <jtbrenna@uvm.edu>
2014-08-03 22:03:54 -0400

    created files for each page. Added hover effects

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="generator" content="">
+    <meta name="dcterms.created" content="">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Template</title>
+
+    <!--[if IE]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php include 'header.php';?>
+    <?php include 'nav.php';?>
+
+    <div class="panel panel-default col-md-10 col-md-offset-1">
+      <div class="panel-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ullamcorper eros elit, convallis eleifend nunc faucibus id. Vestibulum congue viverra diam, et suscipit turpis tempus vitae. Nullam a turpis sapien. Praesent at metus convallis, lacinia felis at, mollis quam. Phasellus in enim tempus, feugiat tortor sit amet, dictum mi. Etiam convallis varius dui sit amet vulputate. Etiam consequat gravida felis sit amet ornare. Sed euismod ultricies diam, et vestibulum risus sollicitudin in. Donec neque erat, sollicitudin ac sodales ac, adipiscing sed nisl. Nunc lacinia tincidunt lorem ut venenatis. Quisque id sem iaculis, tempus magna vitae, mollis orci. Donec nec orci facilisis, tincidunt sapien vitae, ornare diam. Nullam egestas vulputate orci, eu fermentum diam rutrum ac. Praesent sem velit, pellentesque sed sapien sit amet, feugiat consequat arcu.</p>
+        <p>Curabitur eu tortor vitae metus luctus pharetra id ac elit. Praesent dui sem, aliquam eget aliquam sed, dignissim nec urna. Cras id nisl a sapien blandit tincidunt vitae suscipit sapien. Praesent eleifend nunc diam, aliquet eleifend odio fringilla id. Nulla congue viverra libero, vel interdum erat fermentum at. Curabitur ornare orci arcu. Nunc feugiat lacinia neque eget accumsan. Phasellus ornare tellus sed elit molestie, id eleifend tellus tempor. Praesent mollis malesuada elementum. Vestibulum vitae cursus tellus. Cras rutrum adipiscing odio, at tempor erat ultrices at. Aenean ultricies semper diam, eget sollicitudin justo laoreet a.</p>
+        <p>Mauris ut dolor justo. Vestibulum sit amet pulvinar nibh. Morbi ut augue vitae metus sagittis feugiat a sed leo. Ut vulputate laoreet urna in molestie. Nam consequat tortor nec consequat vestibulum. In eu nisi sit amet tortor convallis sagittis. Suspendisse rutrum arcu non orci cursus, id laoreet risus dignissim. Pellentesque semper posuere enim sed tincidunt.</p>
+      </div>
+    </div>
+
+  </body>
+</html>
--- nav.php
+++ nav.php
@@ -8,37 +8,81 @@
     <a class="navbar-brand" href="#">Brand</a>
   </div>
   <div class="navbar-collapse collapse navbar-responsive-collapse">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Active</a></li>
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li class="dropdown-header">Dropdown header</li>
-          <li><a href="#">Separated link</a></li>
-          <li><a href="#">One more separated link</a></li>
-        </ul>
-      </li>
-    </ul>
-    <form class="navbar-form navbar-left">
-      <input type="text" class="form-control col-lg-8" placeholder="Search">
+	<ul class="nav navbar-nav">
+<?php 
+if(basename($_SERVER['PHP_SELF'])=="home.php"){
+    print '<li class="active"><a href="#">Home</a></li>';
+} else {
+    print '<li class="grow"><a href="home.php">Home</a></li>';
+} 
+?>
+
+<?php 
+if(basename($_SERVER['PHP_SELF'])=="history.php"){
+    print '<li class="active"><a href="#">History</a></li>';
+} else {
+    print '<li class="grow"><a href="history.php">History</a></li>';
+} 
+?>
+
+<?php 
+if(basename($_SERVER['PHP_SELF'])=="registration.php"){
+    print '<li class="active"><a href="#">Registration</a></li>';
+} else {
+    print '<li class="grow"><a href="registration.php">Registration</a></li>';
+} 
+?>
+
+<?php 
+if(basename($_SERVER['PHP_SELF'])=="program.php"){
+    print '<li class="active"><a href="#">Program</a></li>';
+} else {
+    print '<li class="grow"><a href="program.php">Program</a></li>';
+} 
+?>  
+
+<?php 
+if(basename($_SERVER['PHP_SELF'])=="locations.php"){
+    print '<li class="active"><a href="#">Locations</a></li>';
+} else {
+    print '<li class="grow"><a href="locations.php">Locations</a></li>';
+} 
+?> 
+
+<?php 
+if(basename($_SERVER['PHP_SELF'])=="travel.php"){
+    print '<li class="active"><a href="#">Travel &amp; Hotels</a></li>';
+} else {
+    print '<li class="grow"><a href="travel.php">Travel &amp; Hotels</a></li>';
+} 
+?>
+
+<?php 
+if(basename($_SERVER['PHP_SELF'])=="events.php"){
+    print '<li class="active"><a href="#">Events</a></li>';
+} else {
+    print '<li class="grow"><a href="events.php">Events</a></li>';
+} 
+?> 
+
+<?php 
+if(basename($_SERVER['PHP_SELF'])=="sponsors.php"){
+    print '<li class="active"><a href="#">Sponsors</a></li>';
+} else {
+    print '<li class="grow"><a href="sponsors.php">Sponsors</a></li>';
+} 
+?>  
+
+<?php 
+if(basename($_SERVER['PHP_SELF'])=="contact.php"){
+    print '<li class="active"><a href="#">Contact Us</a></li>';
+} else {
+    print '<li class="grow"><a href="contact.php">Contact Us</a></li>';
+} 
+?> 
+</ul>
+    <form class="navbar-form navbar-right">
+      <input type="text" class="form-control col-md-8" placeholder="Search">
     </form>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Separated link</a></li>
-        </ul>
-      </li>
-    </ul>
   </div>
 </div>
